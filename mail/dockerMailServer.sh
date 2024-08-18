83391ca6bd4d624f0ee1a1dc64a7173add71aeef
xuhuabao <39855689+xuhuabao@users.noreply.github.com>
2024-08-18 22:12:06 +0800

    Add files via upload

---
+++ mail/dockerMailServer.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+FQDN="mail.gztech.online"
+DOMAINNAME="gztech.online"
+
+############################  设置主机名 hostname #######################
+cat << EOF > /etc/hostname
+$FQDN
+EOF
+
+echo "/etc/hosts: "
+cat /etc/hosts
+#cat << EOF > /etc/hosts
+#127.0.0.1	 localhost
+## 127.0.1.1  $FQDN mail
+#::1		localhost ip6-localhost ip6-loopback
+#ff02::1		ip6-allnodes
+#ff02::2		ip6-allrouters
+#EOF
+
+# 立即应用新的主机名设置，而无需重启系统
+hostnamectl set-hostname $FQDN
+echo -n "hostname: " && hostname
+echo -n "hostname -f (FQDN): " && hostname -f
+
+########################## 启动mailserver容器 ##########################
+PassWord=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
+# 上传 compose.yaml
+wget "https://raw.githubusercontent.com/docker-mailserver/docker-mailserver/master/mailserver.env"
+docker compose up -d
+docker exec -ti mailserver setup email add january@gztech.online ${PassWord}
+docker exec -ti mailserver setup email add postmaster@gztech.online
+docker exec -it mailserver setup alias add postmaster@gztech.online 3096459788@qq.com
+docker exec -it mailserver setup email list
+
+###################### DNS记录 数字签名 dkim ##########################
+docker exec -ti mailserver setup config dkim
+echo "dkim记录"
+cat ./docker-data/dms/config/opendkim/keys/${DOMAINNAME}/mail.txt
+docker compose down mailserver
+echo "添加DNS记录 dkim"
+
+############################# 启动容器 mailserver #############################
+docker compose up --build -d
+docker exec -it mailserver setup email list
+#echo -e "\n" && tail -n 30 ./docker-data/dms/mail-logs/mail.log
+echo ${PassWord}
+
+# 常用命令
+# docker ps -a
+# docker compose down mailserver
+# docker compose up --build -d
+# docker logs --tail 30 mailserver
+# docker restart mailserver
+
+# docker exec -it mailserver setup
+# docker exec -it mailserver cat /etc/dovecot/conf.d/10-master.conf
+# docker exec -it mailserver ls /etc/letsencrypt/live
+# docker exec -ti mailserver setup email add january@gztech.online ${PassWord}
+# docker exec -it mailserver setup email update january@gztech.online ${PassWord}
+# docker exec -it mailserver setup email del january@gztech.online
+# 测试SSL
+# docker exec mailserver openssl s_client -connect 0.0.0.0:465 -starttls smtp -CApath /etc/letsencrypt/
---
+++ mail/myCertbot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 检查是否提供了域名参数
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <domain>"
+    exit 1
+fi
+
+FQDN=$1
+
+apt update
+apt install certbot
+certbot certonly --standalone -d $FQDN --email 3096459788@qq.com --agree-tos
+
+ARCHIVE_DIR="/etc/letsencrypt/archive/${FQDN}"
+DEST_DIR="/etc/letsencrypt/live/${FQDN}"
+
+ls DEST_DIR # 当前活动证书的 符号链接，供服务器和应用程序使用。
+rm -f "${DEST_DIR}/*" # 移除所有 符号链接
+ls ARCHIVE_DIR  # 所有版本的实际证书文件和私钥文件，历史档案
+
+LATEST_VERSION=$(find ${ARCHIVE_DIR} -name 'cert*.pem' -printf '%f\n' | sed -r 's/cert([0-9]+).pem/\1/' | sort -n | tail -n 1)
+echo "LATEST_VERSION: ${LATEST_VERSION}"
+
+cp ${ARCHIVE_DIR}/cert${LATEST_VERSION}.pem ${DEST_DIR}/cert.pem
+cp ${ARCHIVE_DIR}/privkey${LATEST_VERSION}.pem ${DEST_DIR}/privkey.pem
+cp ${ARCHIVE_DIR}/chain${LATEST_VERSION}.pem ${DEST_DIR}/chain.pem
+cp ${ARCHIVE_DIR}/fullchain${LATEST_VERSION}.pem ${DEST_DIR}/fullchain.pem
+
+# bash myCertbot.sh mail.taojiang.online
+# bash myCertbot.sh mail.gztech.online
