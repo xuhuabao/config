bf33ec6673426867b705e861211d10cf4381dcec
xuhuabao <39855689+xuhuabao@users.noreply.github.com>
2024-08-18 23:04:17 +0800

    Update hysteria2Install.sh

--- hysteria2Install.sh
+++ hysteria2Install.sh
@@ -5,8 +5,6 @@ bash <(curl -fsSL https://get.hy2.sh/)
 #生成自签名证书
 openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt \
 -subj "/CN=bing.com" -days 36500 && chown hysteria /etc/hysteria/server.key && chown hysteria /etc/hysteria/server.crt
-echo "/etc/hysteria/server.crt"
-echo "/etc/hysteria/server.key"
 
 PW=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
 
